109482b22ac7c6d4dd5f5ca1b1e3354828ea5779
nikolabaska <64651609+nikolabaska@users.noreply.github.com>
2020-11-12 23:01:43 +0100

    Add files via upload

---
+++ Testrunner/Testruner.java
@@ -0,0 +1,15 @@
+package Testrunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+//@CucumberOptions(
+		//features = {"src/test/resources"}, 
+       //  glue = {"stepdefinitions"},
+   // tags= {"@OrderandContact"})
+public class Testruner {
+
+}
